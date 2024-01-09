cca4250e3a48e950afbf15b3dcc13c27ca3ffd5c
Ayush Kumar Tiwari <91795180+Ayush8085@users.noreply.github.com>
2024-01-09 22:40:07 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { GiDiamonds } from "react-icons/gi";
 import { registerUser, validateEmail } from '../services/AuthServices';
+import { toast } from 'react-toastify';
 
 const Register = () => {
     const [formData, setFormData] = useState({
