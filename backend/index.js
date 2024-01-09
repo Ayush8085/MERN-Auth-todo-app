b03b3a58324cb1cce2c8edc654ee8e5050f7b357
Ayush <ayushkumar9601@gmail.com>
2024-01-09 20:08:34 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 // -------------- Middlewares --------------
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', process.env.FRONTEND_URI],
     credentials: true
 }));
 
